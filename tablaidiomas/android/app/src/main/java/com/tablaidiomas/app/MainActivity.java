cabfbccde59667c8128828deda49d0cb455752d8
Gisela Acosta <gacosta@technisys.com>
2020-09-27 20:08:50 -0300

    agrego animaciones

--- tablaidiomas/android/app/src/main/java/com/tablaidiomas/app/MainActivity.java
+++ tablaidiomas/android/app/src/main/java/com/tablaidiomas/app/MainActivity.java
@@ -1,21 +1,59 @@
 package com.tablaidiomas.app;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-    }});
-  }
+        // Agregar animaciones
+        //Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        //TextView deTextView = findViewById(R.id.deTextView);
+        //final TextView codeliaTextView = findViewById(R.id.codeliaTextView);
+        final ImageView logoImageView = findViewById(R.id.imageView4);
+
+        //deTextView.setAnimation(animacion);
+        //codeliaTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View, String>(logoImageView, "logoImageTrans");
+                // pairs[1] = new Pair<View, String>(codeliaTextView, "textTrans");
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                } else {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 4000);
+    }
 }
